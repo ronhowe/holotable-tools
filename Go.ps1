fb8522a7a9d746e7bb7c5f40e28491c8b82bedec
Ron Howe <ronhowe@hotmail.com>
2021-02-07 11:36:32 -0500

    Added ExportToCdf PowerShell script.

---
+++ Go.ps1
@@ -0,0 +1,3 @@
+./ExportToCdf.ps1 -JsonPath "~/source/repos/swccg-card-json/Dark.json" -CdfPath "./Dark.cdf" -Set "Virtual Set 13" -Verbose
+
+./ExportToCdf.ps1 -JsonPath "~/source/repos/swccg-card-json/Light.json" -CdfPath "./Light.cdf" -Set "Virtual Set 13" -Verbose
