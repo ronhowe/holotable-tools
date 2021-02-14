9f31482633e73b87d5c6a98bacd36eb6e9c95fd4
Ron Howe <ronhowe@hotmail.com>
2021-02-13 21:02:35 -0500

    Script Purge

--- SortCdf.ps1
+++ SortCdf.ps1
@@ -1,32 +1,8 @@
-function SortAndSave () {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [string]
-        $CdfInputPath,
+Clear-Host
 
-        [Parameter()]
-        [string]
-        $CdfOutputPath
-    )
+Set-Location -Path $(Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
 
-    if (Test-Path -Path $CdfOutputPath) {
-        Remove-Item $CdfOutputPath -Force
-    }
-
-    if (Test-Path -Path $CdfInputPath) {
-        Get-Content -Path $CdfInputPath |
-        Where-Object {
-            # ($_.StartsWith("card"))
-            ($_.StartsWith("card `"/starwars"))
-        } |
-        Sort-Object |
-        Add-Content -Path $CdfOutputPath
-    }
-    else {
-        Write-Host "Could not find $CdfInputPath"
-    }
-}
+Import-Module -Name "./HolotableTools.psm1" -Force
 
 SortAndSave -CdfInputPath "~/source/repos/holotable-tools/Dark.cdf" -CdfOutputPath "~/source/repos/holotable-tools/Dark.sorted.cdf"
 SortAndSave -CdfInputPath "~/source/repos/holotable/darkside.cdf" -CdfOutputPath "~/source/repos/holotable-tools/darkside.sorted.cdf"
