3dbfe14c8cdc979f1ceb3d56f95db6cec44443a4
Ron Howe <ronhowe@hotmail.com>
2021-02-16 13:40:30 -0500

    Home One: War Room

--- ExportCdf.ps1
+++ ExportCdf.ps1
@@ -3,7 +3,7 @@
 Clear-Host
 
 # TODO : Remove when done iterating.
-Push-Location -Path "~/source/repos/ronhowe/holotable" ; git pull origin consistency ; Pop-Location
+# Push-Location -Path "~/source/repos/ronhowe/holotable" ; git pull origin consistency-light ; Pop-Location
 
 Write-Host "Exporting..."
 
@@ -12,8 +12,8 @@ Push-Location -Path $(Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
 Import-Module -Name "./HolotableTools.psm1" -Force
 
 $seconds = Measure-Command -Expression {
-    Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Dark.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Dark.cdf"
-    # Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Light.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Light.cdf"
+    # Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Dark.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Dark.cdf"
+    Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Light.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Light.cdf"
 
     # Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Dark.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Dark.cdf" -IdFilter 6535 -Debug -Verbose
     # Export-Cdf -JsonPath "~/source/repos/ronhowe/swccg-card-json/Light.json" -CdfPath "~/source/repos/ronhowe/holotable-tools/Light.cdf" -IdFilter 5300 -Debug -Verbose
@@ -30,13 +30,14 @@ $seconds = Measure-Command -Expression {
 
 Write-Host "Export completed in $seconds seconds." -ForegroundColor Green
 
-Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable/darkside.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/darkside.basic.cdf" -ExcludeLegacy
-Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable-tools/Dark.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/Dark.basic.cdf" -ExcludeLegacy
+# Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable/darkside.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/darkside.basic.cdf" -ExcludeLegacy
+# Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable-tools/Dark.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/Dark.basic.cdf" -ExcludeLegacy
 
-# Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable-tools/Light.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/Lightside.basic.cdf"
-# Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable/lightside.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/lightside.basic.cdf"
+Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable/lightside.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/lightside.basic.cdf" -ExcludeLegacy
+Export-BasicCdf -CdfInputPath "~/source/repos/ronhowe/holotable-tools/Light.cdf" -CdfOutputPath "~/source/repos/ronhowe/holotable-tools/Light.basic.cdf" -ExcludeLegacy
 
-code --diff darkside.basic.cdf Dark.basic.cdf
+# code --diff ./darkside.basic.cdf ./Dark.basic.cdf
+code --diff ./lightside.basic.cdf ./Light.basic.cdf
 
 #endregion Hit F5
 
