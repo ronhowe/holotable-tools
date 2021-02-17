ae35154ffc3134286c15683d9e48a36de3dd985a
Ron Howe <ronhowe@hotmail.com>
2021-02-16 19:02:52 -0500

    BB-8 In Black Squadron 1

--- HolotableTools.psm1
+++ HolotableTools.psm1
@@ -109,6 +109,10 @@ function ConvertTo-CdfDestiny {
                     # R2-D2 (Artoo-Detoo)
                     $output = "2 or 5"
                 }
+                5373 {
+                    # BB-8 In Black Squadron 1
+                    $output = "pi"
+                }
                 136 {
                     # Artoo
                     $output = "1 or 6"
